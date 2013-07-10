5b62b740b80c388e04416e109459aeee4215728a
Bink <erik_kemmer@yahoo.de>
2013-07-10 17:24:44 +0200

    Removed Dim3 stuff. Ready to port to Unity now.

--- Game/Data/Scripts/Weapons/Nothing.js
+++
@@ -1,9 +0,0 @@
-script.attachEvent(DIM3_EVENT_CONSTRUCT,'OnConstruct');
-
-function OnConstruct(weap,subEvent,id,tick)
-{
-	weap.model.on=false;
-	weap.model.name="Blank";
-	weap.ammo.initCount=0;
-	weap.ammo.maxCount=0;
-}
